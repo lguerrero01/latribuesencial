2d73e410c205db181f8402d49eff382439690836
Luis <luisguerrero12387@gmail.com>
2021-09-07 09:31:15 -0400

    project started

---
+++ src/app/components/home/decoration/decoration.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-decoration',
+  templateUrl: './decoration.component.html',
+  styles: [
+  ]
+})
+export class DecorationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
