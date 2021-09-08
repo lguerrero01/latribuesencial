8165ce977ecda79339c5687851fb321042c40e1f
Diego Silva <Diegosilva06092001@gmail.com>
2021-09-08 15:17:55 -0400

    update form

--- src/app/components/home/step1/step1.component.ts
+++ src/app/components/home/step1/step1.component.ts
@@ -24,13 +24,14 @@ export class Step1Component implements OnInit {
     name: [ , [ Validators.required] ],
     country: [ , [ Validators.required] ],
     countryRes : [ , [ Validators.required] ],
+    phone : [ , [ Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)] ]
   })
 
   // ======================================
 	//				onInit
 	// ======================================
   ngOnInit(): void {
-    console.log(this.form1.valid)
+    console.log(this.form1.controls)
   }
   
   public valid() {
@@ -38,6 +39,10 @@ export class Step1Component implements OnInit {
   }
 
   public campoEsValido( field: string ) {
+    return this.form1.controls[field].errors 
+            && this.form1.controls[field].touched;
+  }
+  public phoneIsValid( field: any ) {
     return this.form1.controls[field].errors 
             && this.form1.controls[field].touched;
   }
