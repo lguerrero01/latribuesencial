8cb059602b3e1cdd66d47940018c6fa3720e2de6
Diego Silva <Diegosilva06092001@gmail.com>
2021-09-14 16:26:20 -0400

    cliente

--- src/app/components/stepper/stepper.module.ts
+++ src/app/components/stepper/stepper.module.ts
@@ -8,6 +8,7 @@ import { Step3Component } from "./step3/step3.component";
 import { Step4Component } from "./step4/step4.component";
 import { Step1Component } from "./step1/step1.component";
 import { StepperComponent } from "./stepper.component";
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { StepperComponent } from "./stepper.component";
     FormsModule,
     CommonModule,
     StepperRoutingModule,
+    SharedModule
   ],
 })
 export class StepperModule {}
